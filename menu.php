1532109ea025d6c2a0cd0ec019fdef96edec92e3
BezhenarMordukar <t80_01@mail.ru>
2020-11-22 19:25:13 +0300

    fix css

--- menu.php
+++
@@ -1,7 +0,0 @@
-<div class="header">
-	<a href="#">Главная</a>
-	<a href="puzzle.php">Загадки</a>
-	<a href="guess.php">Угадайка</a>
-	<a href="number.php">Угадайка на 2 игрока</a>
-	<a href="password.php">Генератор пароля</a>
-</div>
